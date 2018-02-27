60ad1154b59ea05767ca72a965f29b5e7c919311
Yeferson Gaitan Gomez <yefri97@utp.edu.co>
2018-02-27 17:13:24 -0500

    Create first prototype

--- src/app/icons.module.ts
+++ src/app/icons.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconUser, IconFile, IconBarChart2, IconFileText } from 'angular-feather';
+import {  IconUser,
+          IconFile,
+          IconBarChart2,
+          IconFileText,
+          IconUserPlus,
+          IconFilePlus,
+          IconHeart } from 'angular-feather';
 
 const icons = [
   IconUser,
   IconFile,
   IconBarChart2,
-  IconFileText
+  IconFileText,
+  IconUserPlus,
+  IconFilePlus,
+  IconHeart
 ];
 
 @NgModule({
